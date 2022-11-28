0059d91ae82b7c618028fbfed322b0ddafac9da9
root <godswillbaiyewu@gmail.com>
2022-11-27 16:02:26 -0800

    tests folder creation and base_model fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,46 @@
-#!/usr/bin/python3
-"""Base model module"""
+""" AirBnb project - BaseModel class """
+
 import uuid
-from models.__init__ import storage
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """BaseModel class"""
-    id = ""
-    created_at = datetime.now()
-    updated_at = datetime.now()
-
+    """ This class defines all common attributes/methods for other classes """
     def __init__(self, *args, **kwargs):
-        """Initializes basemodel instances."""
-        if kwargs and not __class__:
-            for i in kwargs.keys():
-                #print(i, type(i))
-                if i not in ["created_at", "updated_at"]:
-                    setattr(self, i, kwargs[i])
-                else:
-                    self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                    self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        """ method constructor """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        if kwargs:
+            form = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                    continue
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value, form)
+                    continue
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(value, form)
+                    continue
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
             self.id = str(uuid.uuid4())
-            storage.new(self)
+            self.created_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-        """Returns the stringrepresentation of a class."""
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """ print a readable string """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Updates the updated_at variable."""
-        self.updated_at = datetime.now()#.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        keys = self.id
-        keys = self.__class__.__name__ + "." + keys
-        (storage.objects)[keys]["updated_at"] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        storage.save()
-        #self.updated_at = datetime.now()
+        """ updates with the current datetime """
+        self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of all attributes."""
